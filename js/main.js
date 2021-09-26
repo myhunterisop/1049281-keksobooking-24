a0f6e740d5e9ce621dfde1792ee7765290e736dd
u.borovkov <u.borovkov@3h.ru>
2021-09-27 00:21:19 +0300

    some math functions add

---
+++ js/main.js
@@ -0,0 +1,27 @@
+const getRandomIntFromRange = function (from, to) {
+  const result = Math.floor(Math.random() * (to - from + 1)) + from; // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+
+  if (from < 0 || to < 0) {
+    return 'Введите положительные числа.';
+  }
+
+  if (to < from) {
+    `Возможно, вы имели в виду от ${to} до ${from}?`;
+    return result;
+  }
+
+  if (to === from) {
+    return 'Введите корректный диапазон.';
+  }
+
+  return result;
+};
+
+getRandomIntFromRange(0, 100);
+
+function randomFloatInteger(min, max, float = 0) { //https://learn.javascript.ru/task/random-int-min-max
+  let rand = min + Math.random() * (max + 1 - min);
+  return rand.toFixed(float)
+}
+
+randomFloatInteger(0, 100, 6);
