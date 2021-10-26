989297338e6592383af084cc64420a4058d2e1a4
u.borovkov <u.borovkov@3h.ru>
2021-10-26 11:44:14 +0300

    main js rework

--- js/main.js
+++ js/main.js
@@ -1,87 +1,80 @@
-const getRandomIntFromRange = (from, to) => {
-  if (from < 0 || to < 0) {
-    throw new Error('Введите положительные числа');
-  }
-
-  if (to < from) {
-    throw new Error(`Возможно, вы имели в виду от ${to} до ${from}?`);
-  }
-
-  if (to === from) {
-    throw new Error('Введите корректный диапазон.');
-  }
-
-  return Math.floor(Math.random() * (to - from + 1)) + from; // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+const getRandomPositiveInteger = (from, to) => {
+  const lower = Math.ceil(Math.min(Math.abs(from), Math.abs(to)));
+  const upper = Math.floor(Math.max(Math.abs(from), Math.abs(to)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Number(Math.floor(result));
 };
 
-
-const getRandomFloatIntFromRange = (min, max, float = 0) => {
-  const rand = min + Math.random() * (max + 1 - min); //https://learn.javascript.ru/task/random-int-min-max
-  return Number(rand.toFixed(float));
+const getRandomPositiveFloat = (from, to, digits = 1) => {
+  const lower = Math.min(Math.abs(from), Math.abs(to));
+  const upper = Math.max(Math.abs(from), Math.abs(to));
+  const result = Math.random() * (upper - lower) + lower;
+  return Number(result.toFixed(digits));
 };
 
-const getAvatarId = (number) => {
+let getAvatarId = (number) => {
   if (String(number).length === 1) {
     number = `0${number}`;
   }
   return number;
 };
 
-const OFFER_TYPES_ARR = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
-const CHECK_ARR = ['12:00', '13:00', '14:00'];
+let offerTypesArr = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+let offerCheckInOutArr = ['12:00', '13:00', '14:00'];
 
-const getSingleRandomItemFromArr = (arr) => arr[~~(Math.random() * arr.length)];
+let getSingleRandomItemFromArr = (arr) => arr[~~(Math.random() * arr.length)];
 
-const FEATURES_ARR = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator'];
-const OFFER_PHOTO_ARR = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
+let offerFeaturesArr = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator'];
+let offerPhotosArr = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
 
-const getRandomItemsFromArr = (arr) => {
-  const RESULT = [];
-  const MAX_LENGTH = arr.length;
-  const NEW_ARRAY_LENGTH = getRandomIntFromRange(1, MAX_LENGTH);
+let getRandomItemsFromArr = (arr) => {
+  const result = [];
+  const maxLength = arr.length;
+  const newArrayLength = getRandomPositiveInteger(1, maxLength);
 
-  for (let index = 0; index < NEW_ARRAY_LENGTH; index++) {
-    const INDEX_OF_EL = getRandomIntFromRange(0, MAX_LENGTH - 1);
-    const EL = arr[INDEX_OF_EL];
+  for (let index = 0; index < newArrayLength; index++) {
+    const indexOfEl = getRandomPositiveInteger(0, maxLength - 1);
+    const el = arr[indexOfEl];
 
-    if (!RESULT.includes(EL)) {
-      RESULT.push(EL);
+    if (!result.includes(el)) {
+      result.push(el);
     }
   }
-  return RESULT;
+  return result;
 };
 
-const LOCATION_LAT = getRandomFloatIntFromRange(35.65000, 35.70000, 5);
-const LOCATION_LNG = getRandomFloatIntFromRange(139.70000, 139.80000, 5);
+let locationLat = getRandomPositiveFloat(35.65000, 35.70000, 5);
+let locationLng = getRandomPositiveFloat(139.70000, 139.80000, 5);
 
-const AD_TITLES_ARR = ['Объявление №1', 'Объявление №2', 'Объявление №3', 'Объявление №4', 'Объявление №5', 'Объявление №6', 'Объявление №7', 'Объявление №8', 'Объявление №9', 'Объявление №10'];
+let offerTitlesArr = ['Объявление №1', 'Объявление №2', 'Объявление №3', 'Объявление №4', 'Объявление №5', 'Объявление №6', 'Объявление №7', 'Объявление №8', 'Объявление №9', 'Объявление №10'];
 
-const AD_DESCRIPTIONS_ARR = ['Евроремонт', 'Прекрасный вид из окна', 'Хорошее соотношение цена / качество', 'Историческая часть города', 'Метро рядом'];
+let offerDescriptionsArr = ['Евроремонт', 'Прекрасный вид из окна', 'Хорошее соотношение цена / качество', 'Историческая часть города', 'Метро рядом'];
 
-const NUMBER_OF_AD = 10;
+let numberOfAdvertisement = 10;
 
-const AD = () => ({
+let advertisement = () => ({
   autor: {
-    avatar: `img/avatars/user${getAvatarId(getRandomIntFromRange(1, 10))}.png`,
+    avatar: `img/avatars/user${getAvatarId(getRandomPositiveInteger(1, 10))}.png`,
   },
   offer: {
-    title: getSingleRandomItemFromArr(AD_TITLES_ARR),
-    address: String(`${LOCATION_LAT}, ${LOCATION_LNG}`),
-    price: getRandomIntFromRange(1, 50000),
-    type: getSingleRandomItemFromArr(OFFER_TYPES_ARR),
-    rooms: getRandomIntFromRange(1, 4),
-    guests: getRandomIntFromRange(1, 8),
-    checkin: getSingleRandomItemFromArr(CHECK_ARR),
-    checkout: getSingleRandomItemFromArr(CHECK_ARR),
-    features: getRandomItemsFromArr(FEATURES_ARR),
-    description: getRandomItemsFromArr(AD_DESCRIPTIONS_ARR),
-    photos: getRandomItemsFromArr(OFFER_PHOTO_ARR),
+    title: getSingleRandomItemFromArr(offerTitlesArr),
+    address: String(`${locationLat}, ${locationLng}`),
+    price: getRandomPositiveInteger(1, 50000),
+    type: getSingleRandomItemFromArr(offerTypesArr),
+    rooms: getRandomPositiveInteger(1, 4),
+    guests: getRandomPositiveInteger(1, 8),
+    checkin: getSingleRandomItemFromArr(offerCheckInOutArr),
+    checkout: getSingleRandomItemFromArr(offerCheckInOutArr),
+    features: getRandomItemsFromArr(offerFeaturesArr),
+    description: getRandomItemsFromArr(offerDescriptionsArr),
+    photos: getRandomItemsFromArr(offerPhotosArr),
   },
   location: {
-    lat: LOCATION_LAT,
-    lng: LOCATION_LNG,
+    lat: locationLat,
+    lng: locationLng,
   },
 });
 
-const ALL_ADS = Array.from({ length: NUMBER_OF_AD }, AD);
-console.log(ALL_ADS);
+const advertisementArr = Array.from({ length: numberOfAdvertisement }, advertisement);
+
+advertisementArr();
