c0d4fa13974a80b32da4bcb964b30b4cf6a9d386
u.borovkov <u.borovkov@3h.ru>
2021-10-26 11:11:39 +0300

    linter fix

--- js/main.js
+++ js/main.js
@@ -1,15 +1,23 @@
-const getRandomPositiveInteger = (from, to) => {
-  const LOWER = Math.ceil(Math.min(Math.abs(from), Math.abs(to)));
-  const UPPER = Math.floor(Math.max(Math.abs(from), Math.abs(to)));
-  const RESULT = Math.random() * (UPPER - LOWER + 1) + LOWER;
-  return Number(Math.floor(RESULT));
+const getRandomIntFromRange = (from, to) => {
+  if (from < 0 || to < 0) {
+    throw new Error('Введите положительные числа');
+  }
+
+  if (to < from) {
+    throw new Error(`Возможно, вы имели в виду от ${to} до ${from}?`);
+  }
+
+  if (to === from) {
+    throw new Error('Введите корректный диапазон.');
+  }
+
+  return Math.floor(Math.random() * (to - from + 1)) + from; // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 };
 
-const getRandomPositiveFloat = (from, to, digits = 1) => {
-  const LOWER = Math.min(Math.abs(from), Math.abs(to));
-  const UPPER = Math.max(Math.abs(from), Math.abs(to));
-  const RESULT = Math.random() * (UPPER - LOWER) + LOWER;
-  return Number(RESULT.toFixed(digits));
+
+const getRandomFloatIntFromRange = (min, max, float = 0) => {
+  const rand = min + Math.random() * (max + 1 - min); //https://learn.javascript.ru/task/random-int-min-max
+  return Number(rand.toFixed(float));
 };
 
 const getAvatarId = (number) => {
@@ -30,10 +38,10 @@ const OFFER_PHOTO_ARR = ['https://assets.htmlacademy.ru/content/intensive/javasc
 const getRandomItemsFromArr = (arr) => {
   const RESULT = [];
   const MAX_LENGTH = arr.length;
-  const NEW_ARRAY_LENGTH = getRandomPositiveInteger(1, MAX_LENGTH);
+  const NEW_ARRAY_LENGTH = getRandomIntFromRange(1, MAX_LENGTH);
 
   for (let index = 0; index < NEW_ARRAY_LENGTH; index++) {
-    const INDEX_OF_EL = getRandomPositiveInteger(0, MAX_LENGTH - 1);
+    const INDEX_OF_EL = getRandomIntFromRange(0, MAX_LENGTH - 1);
     const EL = arr[INDEX_OF_EL];
 
     if (!RESULT.includes(EL)) {
@@ -43,8 +51,8 @@ const getRandomItemsFromArr = (arr) => {
   return RESULT;
 };
 
-const LOCATION_LAT = getRandomPositiveFloat(35.65000, 35.70000, 5);
-const LOCATION_LNG = getRandomPositiveFloat(139.70000, 139.80000, 5);
+const LOCATION_LAT = getRandomFloatIntFromRange(35.65000, 35.70000, 5);
+const LOCATION_LNG = getRandomFloatIntFromRange(139.70000, 139.80000, 5);
 
 const AD_TITLES_ARR = ['Объявление №1', 'Объявление №2', 'Объявление №3', 'Объявление №4', 'Объявление №5', 'Объявление №6', 'Объявление №7', 'Объявление №8', 'Объявление №9', 'Объявление №10'];
 
@@ -54,15 +62,15 @@ const NUMBER_OF_AD = 10;
 
 const AD = () => ({
   autor: {
-    avatar: `img/avatars/user${getAvatarId(getRandomPositiveInteger(1, 10))}.png`,
+    avatar: `img/avatars/user${getAvatarId(getRandomIntFromRange(1, 10))}.png`,
   },
   offer: {
     title: getSingleRandomItemFromArr(AD_TITLES_ARR),
     address: String(`${LOCATION_LAT}, ${LOCATION_LNG}`),
-    price: getRandomPositiveInteger(1, 50000),
+    price: getRandomIntFromRange(1, 50000),
     type: getSingleRandomItemFromArr(OFFER_TYPES_ARR),
-    rooms: getRandomPositiveInteger(1, 4),
-    guests: getRandomPositiveInteger(1, 8),
+    rooms: getRandomIntFromRange(1, 4),
+    guests: getRandomIntFromRange(1, 8),
     checkin: getSingleRandomItemFromArr(CHECK_ARR),
     checkout: getSingleRandomItemFromArr(CHECK_ARR),
     features: getRandomItemsFromArr(FEATURES_ARR),
